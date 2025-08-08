f6c609346983c04c99a0b9a34f571031252d36b4
Aditya Prajapati <aditya12398@gmail.com>
2025-08-08 13:04:58 +0530

    Added a small section to get tags contained in the first slide

--- src/pptxio.go
+++ src/pptxio.go
@@ -3,13 +3,35 @@ package main
 import (
 	"fmt"
 	"github.com/moipa-cn/pptx"
+	"regexp"
 )
 
 func main() {
 	pptfile, _ := pptx.ReadPowerPoint("./test.pptx")
-	fmt.Println("Just loaded it.")
-	pptfile.DeletePassWord()
-	fmt.Println("Password deleted.")
+	fmt.Println("Presentation has been loaded successfully.")
+
 	slideno := pptfile.GetSlideCount()
 	fmt.Println("Total number of slides in the presentation: ", slideno)
+
+	for slidepaths := range pptfile.Slides {
+		fmt.Println(slidepaths)
+	}
+	allTags := getAllTags(pptfile.Slides, "(?s)<a(.*?)>")
+	fmt.Println("Length of all the tags: ", len(allTags))
+	for i := range allTags {
+		fmt.Println("Tag ", i+1, ": ", allTags[i])
+	}
+}
+
+func getAllTags(workstring map[string]string, pattern string) []string {
+	var tags []string
+	for key := range workstring {
+		re := regexp.MustCompile(pattern)
+		matches := re.FindAllStringSubmatch(workstring[key], -1)
+		for _, m := range matches {
+			tags = append(tags, m[0])
+		}
+		break
+	}
+	return tags
 }
